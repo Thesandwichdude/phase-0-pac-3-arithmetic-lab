731e148a047932c2fc64e62194c0db04e0548654
Kenneth <lopezkenneth2000@yahoo.com>
2021-06-30 20:33:24 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,70 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    const result = a + b;
+    return result
+}
+
+function subtract(a, b) {
+    const result = a - b;
+    return result
+}
+
+function multiply(a, b) {
+    const result = a * b;
+    return result
+}
+
+function divide(a, b) {
+    const result = a / b;
+    return result
+}
+
+function increment(n) {
+    const result = n + 1;
+    return result
+}
+
+function decrement(n) {
+    const result = n - 1;
+    return result
+}
+
+function makeInt(n) {
+    const result = pareInt(n);
+    return result
+}
+
+function makeInt(n) {
+    const result = parseInt(n);
+    return result
+}
+
+function makeInt(n) {
+    const result = parseFloat(n);
+    return result
+}
+
+function preserveDecimal(n) {
+    const result = parseFloat(n);
+    return result
+}
+
+function preserveDecimal(n) {
+    const result = parseFloat(n);
+    return result
+}
